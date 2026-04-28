02393bd54c4679ac0bb2730957437f5765df71bf
Elimua <cristovao.eliezes@gmail.com>
2026-04-28 12:53:05 -0700

    Fix layout syntax error

--- src/app/servico/[id]/layout.tsx
+++ src/app/servico/[id]/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function ServiceLayout({ children }: { children: ReactNode }) {
-  return ><div className="bg-gray-50/30">{children}</div>;
+  return <div className="bg-gray-50/30">{children}</div>;
 }
